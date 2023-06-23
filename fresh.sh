2735a61eb2a0de1dd023d5758da77d6d553075b9
Kevin Schraith <schraith@gmail.com>
2023-06-23 13:04:52 -0700

    fixed to work with p10k

--- fresh.sh
+++ fresh.sh
@@ -19,7 +19,7 @@ fi
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
-ln -s $HOME/Projects/dotfiles/.zshrc $HOME/.zshrc
+ln -s $DOTFILES/.zshrc $HOME/.zshrc
 
 # Update Homebrew recipes
 brew update
